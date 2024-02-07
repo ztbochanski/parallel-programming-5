4c85223e78819f114e6e5c09a45c0023c491a473
Zachary Bochanski <33264009+ztbochanski@users.noreply.github.com>
2024-02-06 22:42:28 -0800

    Update final_experiment.bash

--- final_experiment.bash
+++ final_experiment.bash
@@ -1,19 +1,12 @@
 #!/bin/bash
-#SBATCH -J ZachTest
-#SBATCH -A cs475-575
-#SBATCH -p classgpufinal
-#SBATCH --constraint=v100
-#SBATCH -o proj05.out
-#SBATCH -e proj05.err
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=bochansz@oregonstate.edu
+#SBATCH -J Options
 
 echo "Number of Trials, Blocksize, MegaTrials/Second, Probability"
 for t in 1024 4096 16384 65536 262144 1048576 2097152
 do
         for b in 8 32 64 128
         do      
-                /usr/local/apps/cuda/cuda-10.1/bin/nvcc -I/usr/local/apps/cuda/cuda-10.1/samples/common/inc/ -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
-                ./proj05
+                /usr/local/apps/cuda/cuda-10.1/bin/nvcc -I/usr/local/apps/cuda/cuda-10.1/samples/common/inc/ -DNUMTRIALS=$t -DBLOCKSIZE=$b -o filename filename.cu
+                ./filename
         done
 done
