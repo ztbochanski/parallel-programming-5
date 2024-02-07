ff2e94f94cc44b864afaf8be4348f80f70d1b7e5
Zachary Bochanski <33264009+ztbochanski@users.noreply.github.com>
2024-02-06 22:41:29 -0800

    Update experiment.bash

--- experiment.bash
+++ experiment.bash
@@ -4,7 +4,7 @@ for t in 1024 4096 16384 65536 262144 1048576 2097152
 do
         for b in 8 32 64 128
         do      
-                /usr/local/apps/cuda/cuda-10.1/bin/nvcc -I/usr/local/apps/cuda/cuda-10.1/samples/common/inc/ -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05  proj05.cu
-                ./proj05
+                /usr/local/apps/cuda/cuda-10.1/bin/nvcc -I/usr/local/apps/cuda/cuda-10.1/samples/common/inc/ -DNUMTRIALS=$t -DBLOCKSIZE=$b -o filename filename.cu
+                ./filename
         done
 done
